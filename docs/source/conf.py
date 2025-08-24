adb6f7b8d12e0d85b04852b12b2d6510b15849b2
doirty1998 <doirty.1998@gmail.com>
2025-08-24 14:41:29 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SOS Peludos Leperos'
+copyright = '2025 - José Manuel Suárez Tocino'
+author = 'José Manuel Suárez Tocino'
 
 release = '0.1'
 version = '0.1.0'
